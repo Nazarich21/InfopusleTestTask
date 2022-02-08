271e07f5626b2eafb781f8a5daf841d4d5287d96
Artemii <nazarenko.a@abcloudz.com>
2022-02-08 22:43:48 +0200

    Initial Commit

---
+++ MapBuilder.App/MapBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MapBuilder.App
+{
+    public class MapBuilder
+    {
+        private SignStep _currentOfficeManagerPosition = new()
+        {
+            Floor = 0,
+            Section = 1
+        };
+
+        public IEnumerable<string> BuildRouteMap(IEnumerable<SignStep> signatureMap)
+        {
+            List<string> result = new List<string>();
+            string elevator;
+
+            foreach (var step in signatureMap) 
+            {
+                elevator = step.Floor % 2 == 0 ? "E1" : "E2";
+                string res = RouteBySteps(_currentOfficeManagerPosition, step) <= RouteByElevator(_currentOfficeManagerPosition, step) ? "S" : elevator;
+                _currentOfficeManagerPosition = step;
+                result.Add(res);
+            }
+
+            return result;
+        }
+
+        public int RouteBySteps(SignStep start, SignStep destination) 
+        {
+            return Math.Abs(start.Floor - destination.Floor) * 2;   
+        }
+
+        public int RouteByElevator(SignStep start, SignStep destination)
+        {
+            return start.Section == destination.Section ? Math.Abs(start.Floor - destination.Floor) + 1 : Math.Abs(start.Floor - destination.Floor) + 2;
+        }
+    }
+}
---
+++ MapBuilder.App/SignStep.cs
@@ -0,0 +1,14 @@
+﻿namespace MapBuilder.App
+{
+    public struct SignStep
+    {
+        public int Floor { get; set; }
+        public int Section { get; set; }
+
+        public SignStep(int floor, int section) 
+        {
+            Floor = floor;
+            Section = section;
+        }
+    }
+}
